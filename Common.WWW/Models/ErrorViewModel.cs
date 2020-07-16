838a6799a3d25e44ed22bc316d0f8611af1dc09b
Xiangrui2019 <xiangrui@aiursoft.com>
2020-07-16 10:25:17 +0800

    init

---
+++ Common.WWW/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Common.WWW.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
